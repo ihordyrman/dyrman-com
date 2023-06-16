3a0564b40df2ffe504c094c978fb0a7ec3655a9a
ihordyrman <ihor.dyrman@outlook.com>
2023-06-16 22:04:10 +0300

    Start point with css

--- HtmlGenerator/Program.fs
+++ HtmlGenerator/Program.fs
@@ -1,7 +1,6 @@
 ﻿open System
 open System.IO
 
-// todo: create a css style file
 // todo: apply styles to different elements
 // todo: add more elements, like images, links, etc
 // todo: convert the whole website to plain html with css (no javascript)
@@ -20,6 +19,8 @@ let post =
     $"<html>
       <head>
       <title>{title}</title>
+      <link rel=\"stylesheet\" type=\"text/css\" href=\"styles.css\">
+      <link href=\"https://fonts.googleapis.com/css2?family=JetBrains+Mono\" rel=\"stylesheet\">
       </head>
       <body>
           <h1>{title}</h1>
