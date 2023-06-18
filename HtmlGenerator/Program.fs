f7facf994ea70ca83da1cd5f07e662ae73eae5ce
ihordyrman <ihor.dyrman@outlook.com>
2023-06-18 22:13:41 +0300

    Update fonts

--- HtmlGenerator/Program.fs
+++ HtmlGenerator/Program.fs
@@ -1,32 +1,40 @@
-﻿open System
-open System.IO
+﻿open Markdig
 
 // todo: apply styles to different elements
 // todo: add more elements, like images, links, etc
 // todo: convert the whole website to plain html with css (no javascript)
 
-let file =
-    File.ReadAllText(@"Source\post1.txt").Split([| '\n' |])
-    |> Array.map (fun x -> $"<p>{x}</p>")
-    |> String.concat ""
+// let file =
+//     File.ReadAllText(@"Source\post1.txt").Split([| '\n' |])
+//     |> Array.map (fun x -> $"<p>{x}</p>")
+//     |> String.concat ""
+//
+// printfn $"%i{file.Length}"
+//
+// let date = DateTime.UtcNow.ToString("yyyy-MM-dd HH:mm:ss")
+// let title = "Post 1"
+//
+// let post =
+//     $"<html>
+//       <head>
+//       <title>{title}</title>
+//       <link rel=\"stylesheet\" type=\"text/css\" href=\"styles.css\">
+//       </head>
+//       <body>
+//           <h1>{title}</h1>
+//           <p>{date}</p>
+//           {file}
+//       </body>
+//       </html>"
+//
+// File.WriteAllText("post1.html", post)
 
-printfn $"%i{file.Length}"
+let markdown =
+    "
+![](https://cdn.packtpub.com/article-hub/articles/5a54ee6c-60a7-49b2-99a7-73ae16f4a8a1_image.png) 
 
-let date = DateTime.UtcNow.ToString("yyyy-MM-dd HH:mm:ss")
-let title = "Post 1"
+Figure 1 – top of ChatGPT response  "
 
-let post =
-    $"<html>
-      <head>
-      <title>{title}</title>
-      <link rel=\"stylesheet\" type=\"text/css\" href=\"styles.css\">
-      <link href=\"https://fonts.googleapis.com/css2?family=JetBrains+Mono\" rel=\"stylesheet\">
-      </head>
-      <body>
-          <h1>{title}</h1>
-          <p>{date}</p>
-          {file}
-      </body>
-      </html>"
+let html = Markdown.ToHtml(markdown)
 
-File.WriteAllText("post1.html", post)
+printfn $"%s{html}"
