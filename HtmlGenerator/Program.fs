77e75be275f938e728f3ea14d3dd62db3cb44472
ihordyrman <ihor.dyrman@outlook.com>
2023-12-05 15:34:14 +0200

    Proof of concept - markdown to html

--- HtmlGenerator/Program.fs
+++ HtmlGenerator/Program.fs
@@ -1,40 +1,76 @@
-﻿open Markdig
+﻿open System
+open System.IO
+open Markdig
 
-// todo: apply styles to different elements
-// todo: add more elements, like images, links, etc
-// todo: convert the whole website to plain html with css (no javascript)
+let file = File.ReadAllText(@"Source\post1.md")
 
-// let file =
-//     File.ReadAllText(@"Source\post1.txt").Split([| '\n' |])
-//     |> Array.map (fun x -> $"<p>{x}</p>")
-//     |> String.concat ""
-//
-// printfn $"%i{file.Length}"
-//
-// let date = DateTime.UtcNow.ToString("yyyy-MM-dd HH:mm:ss")
-// let title = "Post 1"
-//
-// let post =
-//     $"<html>
-//       <head>
-//       <title>{title}</title>
-//       <link rel=\"stylesheet\" type=\"text/css\" href=\"styles.css\">
-//       </head>
-//       <body>
-//           <h1>{title}</h1>
-//           <p>{date}</p>
-//           {file}
-//       </body>
-//       </html>"
-//
-// File.WriteAllText("post1.html", post)
+let getProperties (md: string) : (string * string) list =
+    let mutable canSkip = false
 
-let markdown =
-    "
-![](https://cdn.packtpub.com/article-hub/articles/5a54ee6c-60a7-49b2-99a7-73ae16f4a8a1_image.png) 
+    md.Split([| '\n' |])
+    |> Array.filter (fun x ->
+        if x.StartsWith("---") then
+            canSkip <- not canSkip
+            true
+        else
+            canSkip)
+    |> Array.filter (fun x -> not (x.StartsWith("---")))
+    |> Array.map (fun x -> x.Trim())
+    |> Array.filter (fun x -> not (String.IsNullOrEmpty x))
+    |> Array.map (fun x -> x.Split ": ")
+    |> Array.map (fun x -> (x[0], x[1]))
+    |> Array.toList
 
-Figure 1 – top of ChatGPT response  "
+let getPropertyByName properties name = properties |> List.filter (fun (x, _) -> x = name) |> List.head |> snd
 
-let html = Markdown.ToHtml(markdown)
 
-printfn $"%s{html}"
+let split (separator: char) (str: string) = str.Split([| separator |]) |> Array.toList
+let toLower (str: string) = str.ToLowerInvariant()
+
+let convertTextToUrl (text: string) =
+    text
+    |> toLower
+    |> split ' '
+    |> List.map (fun x -> x.Trim())
+    |> List.filter (fun x -> not (String.IsNullOrEmpty x))
+    |> String.concat "-"
+
+let content =
+    let mutable printable = true
+
+    file.Split([| '\n' |])
+    |> Array.filter (fun x ->
+        if x.StartsWith("---") then
+            printable <- not printable
+            false
+        else
+            printable)
+    |> String.concat ""
+
+let properties = getProperties file
+
+let date = getPropertyByName properties "date"
+let title = getPropertyByName properties "page-title"
+let url = getPropertyByName properties "url"
+
+let htmlContent = Markdown.ToHtml(content)
+
+let htmlTemplate =
+    $"""
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>{title}</title>
+    <link rel="stylesheet" type="text/css" href="styles.css">
+</head>
+<body>
+    <h1>{title}</h1>
+    <p>{date}</p>
+    {htmlContent}
+</body>
+</html>
+"""
+
+File.WriteAllText($@"..\..\..\Outputs\{url}.html", htmlTemplate, System.Text.Encoding.UTF8)
+
+printfn $"%s{htmlTemplate}"
